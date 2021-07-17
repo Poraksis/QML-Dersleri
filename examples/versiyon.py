68367496a2ed430b101582ea3dcbb3c0122ac843
Poraksis <onurar30@gmail.com>
2021-07-17 14:37:04 +0300

    Delete versiyon.py

--- examples/versiyon.py
+++
@@ -1,8 +0,0 @@
-#PySide Çekirdek araçlarını yükledik
-import PySide6.QtCore
-
-# PySide6 versiyonunu çıktılar
-print(PySide6.__version__)
-
-# PySide6 yı compile edecek Qt versiyonunu çıktılar.
-print(PySide6.QtCore.__version__)
