32535303e567faa3ca724e7f14c0267fdaca0e96
Poraksis <onurar30@gmail.com>
2021-07-17 14:36:54 +0300

    Create versiyon.py

---
+++ examples/versiyon/versiyon.py
@@ -0,0 +1,8 @@
+#PySide Çekirdek araçlarını yükledik
+import PySide6.QtCore
+
+# PySide6 versiyonunu çıktılar
+print(PySide6.__version__)
+
+# PySide6 yı compile edecek Qt versiyonunu çıktılar.
+print(PySide6.QtCore.__version__)
